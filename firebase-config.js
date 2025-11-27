c645f87720fe5c698488fd0aff629438eb3a4022
amin8914 <amin8914@gmail.com>
2025-11-27 15:39:36 +0000

    migration script and firebase setup

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqTiexOTxz7yp0PdSttvQmTAJzQdZMI-Y",
+  authDomain: "aura-mebel-7ec96.firebaseapp.com",
+  projectId: "aura-mebel-7ec96",
+  storageBucket: "aura-mebel-7ec96.appspot.com",
+  messagingSenderId: "149768023865",
+  appId: "1:149768023865:web:7e9fbd950241375d6a02e8",
+  measurementId: "G-5YG8EC89CY"
+};
